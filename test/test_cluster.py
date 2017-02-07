4e1ce34a338c9ffd8e92a7751e00e1dbf7f8b668
ken_burke <kennethjburkejr@gmail.com>
2017-02-06 19:38:07 -0800

    update similarity testing

--- test/test_cluster.py
+++ test/test_cluster.py
@@ -10,7 +10,8 @@ def test_similarity():
     activesite_b = io.read_active_site(filename_b)
 
     # Reflexive
-    assert cluster.compute_similarity(activesite_a, activesite_a) > 0.99
+    sim_ref, metrics_ref = cluster.compute_similarity(activesite_a, activesite_a)
+    assert sim_ref > 0.99
     
     # Symmetric
     sim_x, metrics_x = cluster.compute_similarity(activesite_a, activesite_b)
