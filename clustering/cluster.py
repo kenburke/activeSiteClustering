800c72b681a35d06310e57e634ded2068717ab58
ken_burke <kennethjburkejr@gmail.com>
2017-02-07 20:53:28 -0800

    cluster_by_partition fully functional

--- clustering/cluster.py
+++ clustering/cluster.py
@@ -3,46 +3,60 @@ from .io import read_active_sites
 import numpy as np
 from random import seed
 
-def cluster_by_partitioning(active_sites,num_clusters=3, max_iters=10000, dist_thresh=0.01):
+def cluster_by_partitioning(active_sites,num_clusters=3, max_iters=10000, dist_thresh=0.001):
     """
     Cluster a given set of ActiveSite instances using a partitioning method.
 
-    Input: a list of ActiveSite instances, (OPTIONAL: number of clusters, default 5)
+    Input: a list of ActiveSite instances
+    (OPTIONAL): number of clusters, maximum iterations, and distance threshold
     Output: a clustering of ActiveSite instances
             (this is really a list of clusters, each of which is list of
             ActiveSite instances)
     """
     
-    cluster_centers = k_means(active_sites,num_clusters,max_iters,dist_thresh)
+    labels, cluster_centers = k_means(active_sites,num_clusters,max_iters,dist_thresh)
     
-    return []
+    clustering = []
+    
+    for clust in range(num_clusters):
+        clustering.append([active_sites[i] for i in range(len(labels)) if labels[i]==clust])
+    
+    return clustering, labels
     
 def k_means(active_sites,num_clusters,max_iters,dist_thresh):
     """
     K-Means clustering
     
     Input: List of active site instances, number of clusters and number of iterations
-    Output: List of cluster centers as numpy arrays
+    Output: List of labels and cluster centers as numpy arrays
     """
     
     # edge cases
     num_clusters = round(num_clusters)
     max_iters = round(max_iters)
+    print('------')
     if num_clusters>len(active_sites) or num_clusters<1:
         print("Invalid number of clusters: Default to 5")
         num_clusters = 5
+    else:
+        print('Number of Clusters:',num_clusters)
     
     # initialize centroids randomly by choosing X random points
     seed()
+    iter = 0
     inds = np.random.choice(len(active_sites),num_clusters,replace=False)
     cluster_centers = np.array([active_sites[i].get_norm_metrics() for i in inds])
     
-    # init trackers
-    iter = 0
+    # init trackers (10 most recent ones)
     prev_centers = np.zeros(cluster_centers.shape)
-    
+
     # begin algorithm
+    print('Maximum Iterations:',max_iters)
+    print('Distance Threshold:',dist_thresh)
+    print('------')
+
     while not iter > max_iters and not distance_cutoff(prev_centers,cluster_centers,dist_thresh):
+                            
         prev_centers = cluster_centers
         iter += 1
         if iter%1000==0:
@@ -52,8 +66,9 @@ def k_means(active_sites,num_clusters,max_iters,dist_thresh):
         labels = assign_to_clusters(active_sites,cluster_centers)
         cluster_centers = update_cluster_locs(active_sites, labels, num_clusters)
     
-        
-    return cluster_centers
+    print('Total iterations:',iter,'\n')    
+    
+    return labels, cluster_centers
 
 def distance_cutoff(prev,current,thresh):
     """
@@ -69,24 +84,23 @@ def distance_cutoff(prev,current,thresh):
     sum /= prev.shape[0]
     
     if sum < thresh:
-        print("Threshold reached, mean center distance travelled is ", sum)
+        print("Threshold reached, mean center distance travelled this step is ", sum)
     
-    return sum < thresh
-    
+    return sum < thresh    
         
 def assign_to_clusters(active_sites,cluster_centers):
     """
     Input: List of ActiveSite instances, and 2D numpy array of cluster centers
-    Output: List of labels, value is the row (cluster) that Site was assigned to.
+    Output: array of labels, value is the row (cluster) that Site was assigned to.
     """
     
     labels = np.zeros([len(active_sites)])  
     
+    #for each active site
     for i in range(len(active_sites)):
-        site_metrics = active_sites[i].get_norm_metrics()
-        cluster_matches = np.sqrt(((cluster_centers - site_metrics)**2).sum(axis=1))
-        labels[i] = np.argmax(cluster_matches)
-        
+        site_metrics = active_sites[i].get_norm_metrics()   #pull its metrics
+        cluster_dist = np.sqrt(((cluster_centers - site_metrics)**2).sum(axis=1)) #get distances
+        labels[i] = np.argmin(cluster_dist)         #label according to closest one
     return labels
 
 def update_cluster_locs(active_sites, labels, num_clusters):
@@ -103,6 +117,8 @@ def update_cluster_locs(active_sites, labels, num_clusters):
         all_clust_metrics = np.array([active_sites[i].get_norm_metrics() for i in site_inds])
         new_center = all_clust_metrics.mean(axis=0)
         new_cluster_centers[clust,:] = new_center
+    
+    return new_cluster_centers
 
 def cluster_hierarchically(active_sites):
     """
